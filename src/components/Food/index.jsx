5ca19529765f1dd6d7c2f085ca1c33be5c5cef36
Carlos Rodrigues <crodrigsantos@gmail.com>
2021-06-07 23:45:38 -0300

    Refactoring pt 1

--- src/components/Food/index.jsx
+++ src/components/Food/index.jsx
@@ -14,6 +14,7 @@ class Food extends Component {
     };
   }
 
+  //Muda avaible para false
   toggleAvailable = async () => {
     const { food } = this.props;
     const { isAvailable } = this.state;
